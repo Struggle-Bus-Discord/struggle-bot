40effb6a06efb098e20b8d493aad97c6e18e5b6e
Gregory Shayko <greg@shaykos.com>
2020-07-24 01:03:00 -0400

    improvements

--- src/features/team-channels.js
+++ src/features/team-channels.js
@@ -34,12 +34,10 @@ export default {
                     let currentChannel = allChannels[i]
                     let previousChannel = allChannels[i-1]
 
-                    log.debug(currentChannel)
-
-                    log.debug("current: " + currentChannel.name + ":" + currentChannel.members.size)
-                    log.debug("previous: " + previousChannel.name + ":" + previousChannel.members.size)
-
-                    if(previousChannel){
+                    if(currentChannel && previousChannel){
+                        log.debug("current: " + currentChannel.name + ":" + currentChannel.members.size)
+                        log.debug("previous: " + previousChannel.name + ":" + previousChannel.members.size)
+                        
                         if(currentChannel.members.size == 0 && previousChannel.members.size == 0){
                             log.info("Deleting channel " + channel.name)
                             currentChannel.delete()
