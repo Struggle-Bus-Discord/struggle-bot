0bdd4dd0f16a8a1cdaaba0119067a8a677d780b7
Gregory Shayko <greg@shaykos.com>
2020-07-24 01:05:19 -0400

    improvements

--- src/features/team-channels.js
+++ src/features/team-channels.js
@@ -31,13 +31,17 @@ export default {
                 let allChannels = channel.parent.children
                 log.debug(allChannels.size)
                 for (var i = allChannels.size - 1; i >= 0; i--) {
+
+                    log.debug("Checking item " + i)
                     let currentChannel = allChannels[i]
+                    log.debug(currentChannel)
                     let previousChannel = allChannels[i-1]
+                    log.debug(previousChannel)
 
                     if(currentChannel && previousChannel){
                         log.debug("current: " + currentChannel.name + ":" + currentChannel.members.size)
                         log.debug("previous: " + previousChannel.name + ":" + previousChannel.members.size)
-                        
+
                         if(currentChannel.members.size == 0 && previousChannel.members.size == 0){
                             log.info("Deleting channel " + channel.name)
                             currentChannel.delete()
