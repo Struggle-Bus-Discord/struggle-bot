3027142b7d7c4f28b44dccc6d056831ef09273ca
Gregory Shayko <greg@shaykos.com>
2021-06-25 13:52:19 -0400

    Update ecosystem.config.example.cjs

--- ecosystem.config.example.cjs
+++ ecosystem.config.example.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   apps : [{
     script: 'src/bot.js',
-    node_args: "--experimental-modules",
+    node_args: "--experimental-modules --require=esm",
     env: {
+      NODE_ENV: 'development',
       "BOT_TOKEN": "<REDACTED>",
+    },
+    env_production: {
+      NODE_ENV: 'production'
     }
+    
   }]
 };
